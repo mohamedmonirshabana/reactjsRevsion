aa359e9a4367b33541be387d3bf463e997321447
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-12 15:31:19 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import Post from './components/Post';
+import PostsList from './components/PostsList';
 function App() {
   return (
     <div className="post">
-      <Post author="Maximilian" body="Reactjs is awesome!" />
-      <Post author="Manual" body="Next JS 's Development" />
+      <PostsList />
     </div>
   );
 }

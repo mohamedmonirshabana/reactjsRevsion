8678146386b7c23835637bb6506988c230c0d7d4
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-12 20:33:31 +0300

    accept al prarmerter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
 import PostsList from './components/PostsList';
+import MainHeader from './components/MainHeader';
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
   return (
-    <div className="post">
-      <PostsList />
-    </div>
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostsList
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
+    </>
   );
 }
 

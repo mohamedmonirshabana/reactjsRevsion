182f019754977369d52ab3eb65d5a9cdf21a7223
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-12 15:41:31 +0300

    add Post List

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,15 @@
 import Post from './Post';
 import classes from './PostList.module.css';
+import NewPost from './NewPost.jsx';
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximilian" body="Reactjs is awesome!" />
-      <Post author="Manual" body="Next JS 's Development" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Maximilian" body="Reactjs is awesome!" />
+        <Post author="Manual" body="Next JS 's Development" />
+      </ul>
+    </>
   );
 }
 export default PostsList;

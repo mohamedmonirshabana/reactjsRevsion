dcf60e8d5a1c214411511f0b4b1be0d2574f1ffe
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-13 11:11:21 +0300

    add Link by Old Way

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,7 +1,7 @@
 import { MdPostAdd, MdMessage } from 'react-icons/md';
 import classes from './MainHeader.module.css';
 
-function MainHeader({ onCreatePost }) {
+function MainHeader() {
   return (
     <header className={classes.header}>
       <h1 className={classes.logo}>
@@ -9,10 +9,10 @@ function MainHeader({ onCreatePost }) {
         React Poster
       </h1>
       <p>
-        <button className={classes.button} onClick={onCreatePost}>
+        <a className={classes.button} href="/create-post">
           <MdPostAdd size={18} />
           New Post
-        </button>
+        </a>
       </p>
     </header>
   );

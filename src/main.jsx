3a4da35148a6442ca6655c790c20c96ab257ccbe
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-13 10:04:41 +0300

    add Some Element for Route

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import NewPost from './components/NewPost';
 
-const router = createBrowserRouter([{ path: '/', element: <App /> }]);
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/create-post', element: <NewPost /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

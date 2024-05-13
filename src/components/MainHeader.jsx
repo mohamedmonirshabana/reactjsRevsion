743758843423360dbee853cd9bc1d331f2aea942
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-13 11:18:39 +0300

    remove A and Add Link because a will reload page and component Again but Link not do that

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { MdPostAdd, MdMessage } from 'react-icons/md';
 import classes from './MainHeader.module.css';
 
@@ -9,10 +10,10 @@ function MainHeader() {
         React Poster
       </h1>
       <p>
-        <a className={classes.button} href="/create-post">
+        <Link className={classes.button} to="/create-post">
           <MdPostAdd size={18} />
           New Post
-        </a>
+        </Link>
       </p>
     </header>
   );

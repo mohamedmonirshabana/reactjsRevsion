9e292eda9f8b258c52d327a3289ba1065291b0d2
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-13 10:18:34 +0300

    add Qutlet for Make a parent Render the Children

--- src/routes/RootLayout.jsx
+++ src/routes/RootLayout.jsx
@@ -1,9 +1,12 @@
+import { Qutlet } from 'react-router-dom';
+
 import MainHeader from '../components/MainHeader';
 
 function RootLayout() {
   return (
     <>
       <MainHeader />
+      <Qutlet />
     </>
   );
 }
